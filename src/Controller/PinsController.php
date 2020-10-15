7844c36629f6dc084c9178d18e7220449d507a92
nsk1703 <kevinsamuelndoum@gmail.com>
2020-10-15 16:38:39 +0200

    Add Show Pages end naigation between pages

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PinsController extends AbstractController
@@ -11,11 +13,19 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      * @param PinRepository $pinRepository
-     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository): Response
     {
         $pins = $pinRepository->findAll();
         return $this->render('pins/index.html.twig', compact('pins'));
+    }
+
+    /**
+     * @Route("/pins/{id<[0-9]+>}", name="app_pins_show")
+     * @param Pin $pin
+     */
+    public function show(Pin $pin): Response
+    {
+        return $this->render('pins/show.html.twig', compact('pin'));
     }
 }
