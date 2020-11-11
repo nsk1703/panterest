56158d71d59790353031737794946e57a725246b
nsk1703 <kevinsamuelndoum@gmail.com>
2020-11-11 11:55:47 +0100

    Add VichUploaderBundle and edit config

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -6,10 +6,13 @@ use App\Entity\Traits\Timestampable;
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
  * @ORM\Table(name="pins")
+ * @Vich\Uploadable()
  * @ORM\HasLifecycleCallbacks()
  */
 class Pin
@@ -35,6 +38,13 @@ class Pin
      * @Assert\Length(min="10")
      */
     private $description;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property
+     *
+     * @Vich\UploadableField(mapping="pin_image", fileNameProperty="imageName", size="")
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -68,6 +78,33 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * If manually uploading a file (i.e not using symfony Form) ensure an instance
+     * of "uploadedFile" is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will one here during
+     * Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null ===! $imageFile)
+        {
+//            It is required that at least one field image if you are using doctrine
+//            otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
     }
 
     public function getImageName(): ?string
