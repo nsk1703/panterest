aed2f46a28fe17d54fcabba26888f28b32afea36
nsk1703 <kevinsamuelndoum@gmail.com>
2020-11-13 06:50:16 +0100

    Add validation for image size

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -44,6 +44,7 @@ class Pin
      * NOTE: This is not a mapped field of entity metadata, just a simple property
      *
      * @Vich\UploadableField(mapping="pin_image", fileNameProperty="imageName")
+     * @Assert\Image(maxSize="1M")
      */
     private $imageFile;
 
